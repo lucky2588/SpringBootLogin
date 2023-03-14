85c490d127fd4d544d928d14ff02d62612155f0e
lucky2588 <duwcsthang2001@gmail.com>
2023-03-14 21:31:49 +0700

    Update UserDB.java

--- src/main/java/com/example/login/db/UserDB.java
+++ src/main/java/com/example/login/db/UserDB.java
@@ -1,24 +1,22 @@
-//package com.example.login.db;
-//
-//import com.example.login.model.User;
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//@Getter
-//@Setter
-//
-//public class UserDB {
-//
-//    // tạo sẵn 1 trường là listUser của UserDB rồi fake tạm Data
-//    public static List<User> listUser = new ArrayList<>(List.of(
-//            new User(1, "DucThang", "123", "thang123@gmail.com", 1222222666, "Hà Nội"),
-//            new User(2, "Phan Như Hương", "123", "PhanHuong@gmail.com", 22222, "Thái Bình "),
-//            new User(3, "Lê Phương Anh", "123", "phuongAnhHaNu@gmail.com", 888888, "Vĩnh Phúc"),
-//            new User(4, "Bùi San ", "123", "buisan@gmail.com", 12131928, "Nam Định"),
-//            new User(5, "Trang Nhung", "123", "trungNhung@gmail.com", 344005816, "Hà Nội")
-//    ));
-//
-//
-//}
+package com.example.login.db;
+
+import com.example.login.model.User;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+@Getter
+@Setter
+
+public class UserDB {
+
+    // tạo sẵn 1 trường là listUser của UserDB rồi fake tạm Data
+    public static List<User> users = new ArrayList<>(List.of(
+            new User(1, "Đức Thắng Nguyễn", "duwcsthang2001@gmail.com", "86666666", "Thành Phố Hà Nội", null, "111"),
+            new User(2, "Nguyễn Văn A", "thanggago147@gmail.com", "0123456789", "Tỉnh Nam Định", null, "222"),
+            new User(3, "Bùi Văn B", "vanA@techmaster.vn", "0123456789", "Tỉnh Hưng Yên", null, "333")
+    ));
+
+
+}
