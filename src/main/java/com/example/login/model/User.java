c2df0ccb10ca926fa786f89925e49a3adca97d6f
lucky2588 <duwcsthang2001@gmail.com>
2023-03-14 21:31:34 +0700

    Update User.java

--- src/main/java/com/example/login/model/User.java
+++ src/main/java/com/example/login/model/User.java
@@ -1,19 +1,20 @@
-//package com.example.login.model;
-//
-//import lombok.*;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Getter
-//@Setter
-//@Builder
-//public class User {
-//    private int id;
-//    private String username;
-//    private String password;
-//    private String email;
-//    private int phone;
-//    private String address;
-//
-//}
+package com.example.login.model;
+
+import lombok.*;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+    private String avatar;
+    private String password;
+
+}
