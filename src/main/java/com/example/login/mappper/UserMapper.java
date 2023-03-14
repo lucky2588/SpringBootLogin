091f6e539e9390f5522be7e9aaf0bf1d9419951b
lucky2588 <duwcsthang2001@gmail.com>
2023-03-14 21:31:41 +0700

    Update UserMapper.java

--- src/main/java/com/example/login/mappper/UserMapper.java
+++ src/main/java/com/example/login/mappper/UserMapper.java
@@ -1,18 +1,26 @@
-//package com.example.login.mappper;
-//
-//import com.example.login.dto.UserDTO;
-//import com.example.login.model.User;
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.Setter;
-//import org.springframework.web.bind.annotation.Mapping;
-//
-//@Getter
-//@Setter
-//@AllArgsConstructor
-//
-//public class UserMapper { // đây là class để cv từ Object sang ObjectDTO
-//    public static UserDTO toUserDTO(User user){
-//        return new UserDTO(user.getId(),user.getUsername(), user.getEmail() , user.getPhone() , user.getAddress());
-//    }
-//}
+package com.example.login.mappper;
+
+import com.example.login.dto.UserDTO;
+import com.example.login.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.Mapping;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
+public class UserMapper { // đây là class để cv từ Object sang ObjectDTO
+    public static UserDTO toUserDto(User user) {
+        UserDTO userDto = new UserDTO();
+        userDto.setId(user.getId());
+        userDto.setName(user.getName());
+        userDto.setEmail(user.getEmail());
+        userDto.setPhone(user.getPhone());
+        userDto.setAddress(user.getAddress());
+        userDto.setAvatar(user.getAvatar());
+
+        return userDto;
+    }
+}
